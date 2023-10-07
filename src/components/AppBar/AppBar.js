85be09398b64b4dfa34435875f03846599311bb6
Oleksii Shevchenko <uzlabini@gmail.com>
2023-10-07 11:27:35 +0200

    updated AppBar.js, fixed bug whith logo

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,13 +1,14 @@
 import Navigation from '../Navigation';
 import { Header } from './AppBar.style';
 import logo from '../../assets/images/logo1.png';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default function Appbar() {
   return (
     <Header>
-      <a href="/" aria-label="Logo">
+      <Link to="/" aria-label="Logo">
         <img src={logo} alt="Logo" width="80" height="80" />
-      </a>
+      </Link>
       <Navigation />
     </Header>
   );
